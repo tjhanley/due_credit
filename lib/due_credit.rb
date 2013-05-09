ed0ab58e6bd8d0c98ff0ea9e0a8a204a5664b9d2
Tom Hanley <tjhanleyx@gmail.com>
2013-05-09 13:54:10 -0700

    initial commit

---
+++ lib/due_credit.rb
@@ -0,0 +1,2 @@
+module DueCredit
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '9b6efc465f6e87421b4683d21e1651b74a90e5a21c02bf8b8376591da8dae56a3cb44a6a2c6c81f7fd4174eaf5a1eddf75e5cc5ecfd1b2a8269f0a45cb354869'
